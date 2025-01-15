0d51ae8ce3abd02a354323366436f3e0ae365c65
Utkarsh <utkjaiswal58@gmail.com>
2025-01-16 01:46:50 +0530

    Property search search property function

--- app/services/property_search.py
+++ app/services/property_search.py
@@ -1,3 +1,5 @@
+from models.property import Property
+
 class PropertySearch:
     def __init__(self):
         self.price_index = {}
@@ -5,4 +7,31 @@ class PropertySearch:
         self.type_index = {}
         self.status_index = {"available": set(), "sold": set()}
         self.user_shortlists = {}
+
+
+    def search_properties(self, criteria: dict) -> list[Property]:
+        results = self.status_index["available"]
+
+        if "min_price" in criteria or "max_price" in criteria:
+            min_price = criteria.get("min_price", float("-inf"))
+            max_price = criteria.get("max_price", float("inf"))
+
+            results &= {
+                pid for price, properties in self.price_index.items()
+                if min_price <= price <= max_price for pid in properties
+            }
+
+        if "location" in criteria:
+            results &= self.location_index.get(criteria["location"], set())
+
+        
+        if "property_type" in criteria:
+            results &= self.type_index.get(criteria["property_type"], set())
+
+
+        sorted_results = sorted(
+            results, key=lambda pid: self.price_index[pid]
+        )
+
+        return sorted_results
         
