8e1930586483f233ecb0d2198ba5c22d5a85fe03
Utkarsh <utkjaiswal58@gmail.com>
2025-01-16 01:27:00 +0530

    property manager add property function

--- app/services/property_manager.py
+++ app/services/property_manager.py
@@ -7,3 +7,17 @@ class PropertyManager:
         self.property_storage = {}
         self.user_portfolios = {}
         self.status_index = {"available": set(), "sold": set()}
+
+
+    def add_property(self, user_id: str, property_details: dict) -> str:
+        property_id = str(uuid.uuid4())
+        property_object = Property(property_id, user_id, property_details)
+        self.property_storage[property_id] = property_object
+        
+        if user_id not in self.user_portfolios:
+            self.user_portfolios[user_id] = []
+
+        self.user_portfolios[user_id].append(property_id)
+        self.status_index["available"].add(property_id)
+
+        return property_id
