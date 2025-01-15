88abc1d99a6b59cbfeb6d5b2aa92bdb8a26bb94e
Utkarsh <utkjaiswal58@gmail.com>
2025-01-16 01:40:09 +0530

    updated Property search init function

--- app/services/property_search.py
+++ app/services/property_search.py
@@ -1,5 +1,8 @@
 class PropertySearch:
-    def __init__(self, property_manager):
-        self.property_manager = property_manager
+    def __init__(self):
+        self.price_index = {}
+        self.location_index = {}
+        self.type_index = {}
+        self.status_index = {"available": set(), "sold": set()}
         self.user_shortlists = {}
         
